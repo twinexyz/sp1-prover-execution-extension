c3ea62037559ac8fcc5b81bcbedc6d83f6073c92
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-10-03 12:50:01 +0545

    feat: added poster for posting proofs to L1

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,55 @@
 mod utils;
 use futures_util::StreamExt;
-use reth::
-    api::FullNodeComponents
-;
+use reth::api::FullNodeComponents;
 use reth_exex::{ExExContext, ExExEvent, ExExNotification};
 use reth_node_ethereum::EthereumNode;
 use reth_tracing::tracing::info;
-use std::{process::Command, sync::{Arc, Mutex}, time::Instant};
+use std::{
+    process::Command,
+    sync::{Arc, Mutex},
+    time::Instant,
+};
+mod poster;
+use poster::poster::post_to_l1;
+use dotenv::dotenv;
 
-
-struct CommandMut {
-    command_mutex: u64
-}
-
-
-async fn my_exex<Node: FullNodeComponents>(mut ctx: ExExContext<Node>, cmd_mut: Arc<Mutex<CommandMut>>) -> eyre::Result<()> {
+async fn my_exex<Node: FullNodeComponents>(
+    mut ctx: ExExContext<Node>,
+    cmd_mut: Arc<Mutex<i32>>,
+) -> eyre::Result<()> {
     while let Some(notification) = ctx.notifications.next().await {
         match &notification {
             ExExNotification::ChainCommitted { new } => {
                 let blocks = new.blocks_iter();
-                let mut mut_guard = cmd_mut.lock().unwrap();
-                for block in blocks {
-                    let start_time = Instant::now();
-                    let output = Command::new("rsp").args(["--block-number", &format!("{}", block.block.number), "--rpc-url", "http://localhost:8545/", "--chain-id", "1337", "--prove"]).output().expect("failed to run the process");
-                    println!("************************************************************************");
-                    println!("*******************************exit status******************************: {}", output.status);
-                    println!("************************************************************************");
-                    let elapsed_time = start_time.elapsed();
-                    println!("***********************Total proving time: {:?}secs", elapsed_time.as_secs())
-                 }
-                mut_guard.command_mutex += 1; 
+                {
+                    let mut mut_guard = cmd_mut.lock().unwrap();
+                    // for block in blocks {
+                    //     let start_time = Instant::now();
+                    //     let output = Command::new("rsp")
+                    //         .args([
+                    //             "--block-number",
+                    //             &format!("{}", block.block.number),
+                    //             "--rpc-url",
+                    //             "http://localhost:8545/",
+                    //             "--chain-id",
+                    //             "1337",
+                    //             "--prove",
+                    //         ])
+                    //         .output()
+                    //         .expect("failed to run the process");
+                    //     println!("************************************************************************");
+                    //     println!("*******************************exit status******************************: {}", output.status);
+                    //     println!("************************************************************************");
+                    //     let elapsed_time = start_time.elapsed();
+                    //     println!(
+                    //         "***********************Total proving time: {:?}secs",
+                    //         elapsed_time.as_secs()
+                    //     )
+                    // }
+                    *mut_guard += 1;
+                }
+                // take input from the file 
+                post_to_l1().await;
             }
             ExExNotification::ChainReorged { old, new } => {
                 info!(from_chain = ?old.range(), to_chain = ?new.range(), "Received reorg");
@@ -49,7 +69,8 @@ async fn my_exex<Node: FullNodeComponents>(mut ctx: ExExContext<Node>, cmd_mut: 
 }
 
 fn main() -> eyre::Result<()> {
-    let cmd_mut = Arc::new(Mutex::new(CommandMut{ command_mutex: 0}));
+    dotenv().ok();
+    let cmd_mut = Arc::new(Mutex::new(0));
     reth::cli::Cli::parse_args().run(|builder, _| async move {
         let handle = builder
             .node(EthereumNode::default())
@@ -63,5 +84,3 @@ fn main() -> eyre::Result<()> {
         handle.wait_for_node_exit().await
     })
 }
-
-
---
+++ src/poster/poster.rs
@@ -0,0 +1,65 @@
+use alloy::{
+    hex::{FromHex, ToHexExt},
+    signers::local::PrivateKeySigner,
+};
+use alloy_network::{EthereumWallet, TransactionBuilder};
+use alloy_primitives::{Address, B256, U256};
+use alloy_provider::{Provider, ProviderBuilder};
+use alloy_sol_types::sol;
+
+sol! {
+    #[sol(rpc)] // <-- Important! Generates the necessary `MyContract` struct and function methods.
+    #[sol(bytecode = "6080604052348015600e575f80fd5b506102128061001c5f395ff3fe608060405234801561000f575f80fd5b506004361061004a575f3560e01c80633fb5c1cb1461004e5780638381f58a1461006a578063d09de08a14610088578063f2c9ecd814610092575b5f80fd5b61006860048036038101906100639190610115565b6100b0565b005b6100726100b9565b60405161007f919061014f565b60405180910390f35b6100906100be565b005b61009a6100d6565b6040516100a7919061014f565b60405180910390f35b805f8190555050565b5f5481565b5f808154809291906100cf90610195565b9190505550565b5f8054905090565b5f80fd5b5f819050919050565b6100f4816100e2565b81146100fe575f80fd5b50565b5f8135905061010f816100eb565b92915050565b5f6020828403121561012a576101296100de565b5b5f61013784828501610101565b91505092915050565b610149816100e2565b82525050565b5f6020820190506101625f830184610140565b92915050565b7f4e487b71000000000000000000000000000000000000000000000000000000005f52601160045260245ffd5b5f61019f826100e2565b91507fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff82036101d1576101d0610168565b5b60018201905091905056fea2646970667358221220fc2f623e42449062279b74795e00c806c8bf3b825abc71630e38f067d6162f5a64736f6c63430008190033")] // <-- Generates the `BYTECODE` static and the `deploy` method.
+    contract Counter {
+        constructor(address) {} // The `deploy` method will also include any constructor arguments.
+
+        #[derive(Debug)]
+        function setNumber(uint256 newNumber) public;
+
+        #[derive(Debug)]
+        function increament() public;
+
+        #[derive(Debug)]
+        function getNumber() public view returns (uint256);
+    }
+}
+
+pub async fn post_to_l1() {
+    let contract_address = std::env::var("CONTRACT_ADDRESS").unwrap();
+    let private_key = std::env::var("PRIVATE_KEY").unwrap();
+    let rpc_url = std::env::var("RPC_URL").unwrap();
+
+    let contract_address = Address::from_hex(contract_address).unwrap();
+    let signer = PrivateKeySigner::from_bytes(&B256::from_hex(private_key).unwrap()).unwrap();
+    let wallet = EthereumWallet::from(signer);
+
+    let provider = ProviderBuilder::new()
+        .with_cached_nonce_management()
+        .wallet(wallet.clone())
+        .on_builtin(&rpc_url)
+        .await
+        .unwrap();
+    let contract = Counter::new(contract_address, provider.clone());
+
+    let call_builder = contract.getNumber();
+    let number = call_builder.call().await.unwrap();
+    println!("number is {}", number._0);
+
+    if true {
+        panic!("this is panic")
+    }
+
+    let set_call_builder = contract
+        .setNumber(U256::from(10))
+        .into_transaction_request()
+        .with_gas_limit(250000)
+        .with_chain_id(1337)
+        .with_max_fee_per_gas(2000000000000)
+        .with_max_priority_fee_per_gas(2000000);
+
+    let builder = provider.send_transaction(set_call_builder).await.unwrap();
+
+    let tx_hash = *builder.tx_hash();
+
+    println!("{:?}", tx_hash.encode_hex())
+}
