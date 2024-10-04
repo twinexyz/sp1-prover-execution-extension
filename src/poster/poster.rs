0558117debbd7322a17b09beb916016c56f77bed
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-10-04 09:08:19 +0545

    feat: load proof into the poster

--- src/poster/poster.rs
+++ src/poster/poster.rs
@@ -1,9 +1,6 @@
-use std::sync::mpsc;
+use std::{fs, sync::mpsc};
 
-use alloy::{
-    hex::FromHex,
-    signers::local::PrivateKeySigner,
-};
+use alloy::{hex::FromHex, signers::local::PrivateKeySigner};
 use alloy_network::EthereumWallet;
 use alloy_primitives::{bytes::Bytes, Address, B256};
 use alloy_provider::ProviderBuilder;
@@ -36,10 +33,22 @@ pub async fn post_to_l1(rx: mpsc::Receiver<u64>) {
         let block_number = rx.recv();
         match block_number {
             Ok(bn) => {
-                if true {
-                    _ = bn;
-                    return 
+                let proof_file_path = format!("proofs/execution_proof_{}.proof", bn);
+                let proof_file = fs::File::open(proof_file_path).unwrap();
+                let proof_object: Result<sp1_sdk::SP1ProofWithPublicValues, serde_json::Error> =
+                    serde_json::from_reader(proof_file);
+                match proof_object {
+                    Ok(proof) => {
+                        _ = proof;
+                        println!("proof found")
+                    }
+                    Err(_) => println!("proof not found"),
                 }
+
+                if true {
+                    return;
+                }
+
                 let provider = ProviderBuilder::new()
                     .with_cached_nonce_management()
                     .wallet(wallet.clone())
@@ -50,9 +59,13 @@ pub async fn post_to_l1(rx: mpsc::Receiver<u64>) {
 
                 let byte_value = Bytes::new();
                 let byte_value2 = Bytes::new();
-                let call_builder = contract.verifyProof(alloy_primitives::FixedBytes([0u8;32]), alloy_primitives::Bytes(byte_value), alloy_primitives::Bytes(byte_value2));
+                let call_builder = contract.verifyProof(
+                    alloy_primitives::FixedBytes([0u8; 32]),
+                    alloy_primitives::Bytes(byte_value),
+                    alloy_primitives::Bytes(byte_value2),
+                );
                 _ = call_builder.call().await.unwrap();
-                // TODO: send the proof back to the L1 chain                
+                // TODO: send the proof back to the L1 chain
             }
             Err(_) => break,
         }
