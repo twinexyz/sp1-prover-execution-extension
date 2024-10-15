37d4a4693dc30a833be551ea2def2a84bd10ead5
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-10-15 09:13:12 +0545

    feat: args for passing config file, removed proving logic from main

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,37 @@ use reth::api::FullNodeComponents;
 use reth_exex::{ExExContext, ExExEvent, ExExNotification};
 use reth_node_ethereum::EthereumNode;
 use reth_tracing::tracing::info;
+use serde::{Deserialize, Serialize};
 use std::{
-    process::Command,
+    fs::read_to_string,
+    process::ExitStatus,
     sync::{mpsc, Arc, Mutex},
-    time::Instant, u64,
+    time::Instant,
+    u64,
 };
+use yaml_config::load;
 mod poster;
+mod prover;
+use clap::Parser;
 use dotenv::dotenv;
 use poster::poster::post_to_l1;
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    config: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
+struct Config {
+    prover: prover::prover::Prover,
+}
 
 async fn my_exex<Node: FullNodeComponents>(
     mut ctx: ExExContext<Node>,
     cmd_mut: Arc<Mutex<i32>>,
+    config: Config,
 ) -> eyre::Result<()> {
     while let Some(notification) = ctx.notifications.next().await {
         match &notification {
@@ -25,35 +44,23 @@ async fn my_exex<Node: FullNodeComponents>(
                 {
                     let mut mut_guard = cmd_mut.lock().unwrap();
                     for block in blocks {
-                        _=block;
-                        // if block.transaction_root_is_empty() {
-                        //     println!("always continue?????");
-                        //     tx.send(0).unwrap();
-                        //     continue;
-                        // }
-                        // println!("proof generation started for block {}", block.block.number);
-                        // let start_time = Instant::now();
-                        // let output = Command::new("rsp")
-                        //     .args([
-                        //         "--block-number",
-                        //         &format!("{}", block.block.number),
-                        //         "--rpc-url",
-                        //         "http://localhost:8545/",
-                        //         "--chain-id",
-                        //         "1337",
-                        //         "--prove",
-                        //     ])
-                        //     .output()
-                        //     .expect("failed to run the process");
-                        // println!("************************************************************************");
-                        // println!("*******************************exit status******************************: {}", output.status);
-                        // println!("************************************************************************");
-                        // let elapsed_time = start_time.elapsed();
-                        // println!(
-                        //     "***********************Total proving time: {:?}secs",
-                        //     elapsed_time.as_secs()
-                        // );
-                        tx.send(1500).unwrap();
+                        _ = block;
+                        if block.transaction_root_is_empty() {
+                            println!("always continue?????");
+                            tx.send(0).unwrap();
+                            continue;
+                        }
+                        let start_time = Instant::now();
+                        let exit_status = config.prover.prove(block.block.number);
+                        if !exit_status.success() {
+                            println!("proof generation failed.")
+                        }
+                        let elapsed_time = start_time.elapsed();
+                        println!(
+                            "***********************Total proving time: {:?}secs",
+                            elapsed_time.as_secs()
+                        );
+                        tx.send(block.block.number).unwrap();
                     }
                     *mut_guard += 1;
                 }
@@ -79,13 +86,18 @@ async fn my_exex<Node: FullNodeComponents>(
 
 fn main() -> eyre::Result<()> {
     dotenv().ok();
+    let args = Args::parse();
+
+    let config_file = read_to_string(args.config).unwrap();
+    let config: Config = serde_yaml::from_str(&config_file).unwrap();
+
     let cmd_mut = Arc::new(Mutex::new(0));
     reth::cli::Cli::parse_args().run(|builder, _| async move {
         let handle = builder
             .node(EthereumNode::default())
             .install_exex("my-exex", |ctx| async move {
                 println!("installing exex");
-                Ok(my_exex(ctx, cmd_mut))
+                Ok(my_exex(ctx, cmd_mut, config))
             })
             .launch()
             .await?;
