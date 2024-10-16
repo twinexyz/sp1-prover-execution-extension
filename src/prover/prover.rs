1f039d3e2a7e44b99d7a691b4ff567c89ef2db8b
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-10-16 13:23:08 +0545

    fix: fixed hardcoded values

--- src/prover/prover.rs
+++ src/prover/prover.rs
@@ -2,37 +2,52 @@ use std::process::{Command, ExitStatus};
 
 use serde::{Deserialize, Serialize};
 
+use crate::poster::poster::Poster;
+
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Prover {
     last_proved_block: u64,
     proof_path: String,
     rpc_url: String,
+    pub poster: Poster,
+    chain_id: String, 
 }
 
 impl Prover {
-    pub fn _new(last_proved_block: u64, proof_path: String, rpc_url: String) -> Self {
+    pub fn new(
+        last_proved_block: u64,
+        proof_path: String,
+        rpc_url: String,
+        identifier: String,
+        aggregator_url: String,
+        chain_id: String, 
+    ) -> Self {
         let output = Command::new("which").args(["rsp"]).output().unwrap().stdout;
         if output.len() == 0 {
             panic!("rsp process not found in PATH")
         }
 
+        let poster = Poster::new(aggregator_url, proof_path.clone(), identifier);
+
         Self {
             last_proved_block,
             proof_path,
             rpc_url,
+            poster,
+            chain_id
         }
     }
 
     /// calculates the execution proof of the specified block using rsp and returns the file path.
-    pub fn _prove(&self, block_number: u64) -> ExitStatus {
+    pub fn prove(&self, block_number: u64) -> ExitStatus {
         let output = Command::new("rsp")
             .args([
                 "--block-number",
                 &format!("{}", block_number),
                 "--rpc-url",
-                "http://localhost:8545/",
+                &self.rpc_url,
                 "--chain-id",
-                "1337",
+                &self.chain_id,
                 "--prove",
             ])
             .output()
