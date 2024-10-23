0e98dfd9a300dab2b436715aa1ad3657d716cfe9
parajuliswopnil <parajuliswopnil717@gmail.com>
2024-10-23 12:49:12 +0545

    fix: removed mpsc receiver

--- src/poster/poster.rs
+++ src/poster/poster.rs
@@ -2,7 +2,7 @@ use reqwest::Client;
 use reth_tracing::tracing::error;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use std::{fs, sync::mpsc};
+use std::fs;
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize, Default)]
 pub struct Poster {
@@ -22,74 +22,57 @@ impl Poster {
         }
     }
 
-    pub async fn send_proof_to_aggregator(&self, rx: mpsc::Receiver<u64>) {
-        if true {
-            return;
-        }
-        loop {
-            let block_number = rx.recv();
-            match block_number {
-                Ok(bn) => {
-                    if bn == 0u64 {
-                        continue;
-                    } else if bn == u64::MAX {
-                        return;
-                    }
-                    let proof_file_path =
-                        format!("{}/execution_proof_{}.proof", self.proof_path, bn);
-                    let proof_file = fs::File::open(proof_file_path).unwrap(); // map error // use expect instead of unwrap where you want to actually halt the execution
-                    let proof_object: Result<sp1_sdk::SP1ProofWithPublicValues, serde_json::Error> =
-                        serde_json::from_reader(proof_file);
+    pub async fn send_proof_to_aggregator(&self, block_height: u64) {
+        let proof_file_path = format!("{}/execution_proof_{}.proof", self.proof_path, block_height);
+        let proof_file = fs::File::open(proof_file_path).expect(format!("proof file for block {} not found", block_height).as_str()); 
+        let proof_object: Result<sp1_sdk::SP1ProofWithPublicValues, serde_json::Error> =
+            serde_json::from_reader(proof_file);
 
-                    match proof_object {
-                        Ok(proof_buffer) => {
-                            let payload = json!({
-                                "jsonrpc": "2.0",
-                                "method": "twarb_sendProof",
-                                "params": [
-                                    {
-                                        "type": "SP1Proof",
-                                        "identifier": &self.identifier,
-                                        "proof": proof_buffer
-                                    }
-                                    ],
-                                "id": 1
-                            });
+        match proof_object {
+            Ok(proof_buffer) => {
+                let payload = json!({
+                    "jsonrpc": "2.0",
+                    "method": "twarb_sendProof",
+                    "params": [
+                        {
+                            "type": "SP1Proof",
+                            "identifier": &self.identifier,
+                            "proof": proof_buffer
+                        }
+                        ],
+                    "id": 1
+                });
 
-                            let client = Client::new();
-                            let mut retry = 0u8;
-                            loop {
-                                let response = client
-                                    .post(&self.aggregator_url)
-                                    .json(&payload)
-                                    .send()
-                                    .await;
+                let client = Client::new();
+                let mut retry = 0u8;
+                loop {
+                    let response = client
+                        .post(&self.aggregator_url)
+                        .json(&payload)
+                        .send()
+                        .await;
 
-                                match response {
-                                    Ok(res) => {
-                                        if !res.status().is_success() {
-                                            if retry < MAX_RETRY {
-                                                retry += 1;
-                                                continue;
-                                            } else {
-                                                error!("Proof could not be sent to the aggregator"); // TODO extended retry logic
-                                                break;
-                                            }
-                                        }
-                                        break;
-                                    }
-                                    Err(_) => {
-                                        error!("Proof sending failed.");
-                                        break;
-                                    }
+                    match response {
+                        Ok(res) => {
+                            if !res.status().is_success() {
+                                if retry < MAX_RETRY {
+                                    retry += 1;
+                                    continue;
+                                } else {
+                                    error!("Proof could not be sent to the aggregator"); // TODO extended retry logic
+                                    break;
                                 }
                             }
+                            break;
                         }
-                        Err(_) => error!("proof not found"),
+                        Err(_) => {
+                            error!("Proof sending failed.");
+                            break;
+                        }
                     }
                 }
-                Err(_) => break,
             }
+            Err(_) => error!("proof not found"),
         }
     }
 }
